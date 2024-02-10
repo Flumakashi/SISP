07f6d6937282e01c36c207b722e0b30c9eb936b4
Flumakashi <flumakashi@gmail.com>
2024-02-10 15:15:21 +0300

    main version 10.02.2024 getConnectionDB getDataDB ShowDataTableView

---
+++ DiplomMain/src/main/java/org/example/diplommain/Database.java
@@ -0,0 +1,36 @@
+package org.example.diplommain;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    private static final String dbHost = "localhost";
+    private static final String dbPort = "5432";
+    private static final String dbUser = "postgres";
+    private static final String dbPass = "1209qwpo";
+    private static final String dbName = "SISP";
+    private static final String url = "jdbc:postgresql://" + dbHost + ":" + dbPort + "/" + dbName;
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, dbUser, dbPass);
+    }
+
+    public static List<DataModel> getData(){
+        List<DataModel> dataList = new ArrayList<>();
+        try (Connection connection = getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM sispinfo")) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                int num = resultSet.getInt("num");
+                dataList.add(new DataModel(id, num));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dataList;
+    }
+}
